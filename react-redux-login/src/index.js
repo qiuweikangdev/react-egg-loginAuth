5fd1e4a4775e920d08b454a9b836fa6673c378ee
qqqiu <642827353@qq.com>
2020-03-15 21:40:02 +0800

    chore: 规范commit提交信息

--- react-redux-login/src/index.js
+++ react-redux-login/src/index.js
@@ -3,12 +3,13 @@
  * @version: 
  * @Author: qqqiu
  * @Date: 2020-01-25 16:15:47
- * @LastEditors  : qqqiu
- * @LastEditTime : 2020-01-31 16:32:20
+ * @LastEditors: qqqiu
+ * @LastEditTime: 2020-03-06 16:53:25
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+
 ReactDOM.render( <App/>,  document.getElementById('root'))
 
 
