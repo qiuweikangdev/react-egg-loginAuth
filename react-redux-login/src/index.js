79c2ca5a193b5031ac3672765d15c8f6d54e945f
qqqiu <642827353@qq.com>
2020-03-16 00:56:47 +0800

    Revert "chore: 规范commit提交信息"
    This reverts commit 5fd1e4a4775e920d08b454a9b836fa6673c378ee.

--- react-redux-login/src/index.js
+++ react-redux-login/src/index.js
@@ -3,13 +3,12 @@
  * @version: 
  * @Author: qqqiu
  * @Date: 2020-01-25 16:15:47
- * @LastEditors: qqqiu
- * @LastEditTime: 2020-03-06 16:53:25
+ * @LastEditors  : qqqiu
+ * @LastEditTime : 2020-01-31 16:32:20
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
 ReactDOM.render( <App/>,  document.getElementById('root'))
 
 
