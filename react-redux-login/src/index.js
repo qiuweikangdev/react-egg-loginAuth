9e31e813caf45be27f551b7ec320f613bc25d1b8
qqqiu <642827353@qq.com>
2020-03-16 10:41:55 +0800

    Revert "Revert "Revert "chore: 规范commit提交信息"""
    This reverts commit 17945c50d12c14485a889e4a253abdffb8f17c09.
    
     Changes to be committed:
    	modified:   README.md
    	deleted:    react-redux-login/package-lock.json
            modified:   react-redux-login/package.json
    	modified:   react-redux-login/src/index.js
    	modified:   react-redux-login/src/pages/login/index.jsx

--- react-redux-login/src/index.js
+++ react-redux-login/src/index.js
@@ -3,7 +3,11 @@
  * @version: 
  * @Author: qqqiu
  * @Date: 2020-01-25 16:15:47
+<<<<<<< HEAD
  * @LastEditors  : qqqiufce
+=======
+ * @LastEditors  : qqqiu
+>>>>>>> 454370b... Revert "Revert "Revert "chore: 规范commit提交信息"""
  * @LastEditTime : 2020-01-31 16:32:20
  */
 import React from 'react';
