6a4164d92a20c7cb8d18f4e0d071e56f4e1e866f
qqqiu <642827353@qq.com>
2020-06-17 14:16:58 +0800

    refactor:重构

--- react-redux-login/src/index.js
+++ react-redux-login/src/index.js
@@ -1,18 +1,12 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: qqqiu
  * @Date: 2020-01-25 16:15:47
-<<<<<<< HEAD
  * @LastEditors  : qqqiufce
-=======
- * @LastEditors  : qqqiu
->>>>>>> 454370b... Revert "Revert "Revert "chore: 规范commit提交信息"""
  * @LastEditTime : 2020-01-31 16:32:20
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App'
-ReactDOM.render( <App/>,  document.getElementById('root'))
-
-
+ReactDOM.render(<App />, document.getElementById('root'))
